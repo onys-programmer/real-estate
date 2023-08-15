5b05b70b3e6e18030ecb433c1f76b1757c55c7f8
taehokim-onys <thkim2017@gmail.com>
2023-08-15 20:29:38 +0900

    feat: 🎸 svg home

--- src/constants/iconSrcMap.js
+++ src/constants/iconSrcMap.js
@@ -1,6 +1,6 @@
 const ICON_SRC_MAP = {
   'building': '/images/icons/icon-building-mono.png',
-  'house': 'images/icons/icon-home-mono.png',
+  'house': 'images/icons/icon-home-mono.svg',
 };
 
 export default ICON_SRC_MAP;
